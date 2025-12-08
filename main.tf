2345250b8022635f1be2981b5c82bc0634f4404b
Lucrece-Hendrelle <Lucrecenfono10@gmail.com>
2025-12-08 10:42:03 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,35 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+# Construire l'image Docker à partir du Dockerfile local
+resource "docker_image" "web" {
+  name = "demo-devops"
+  build {
+    context = "."  # dossier du projet
+  }
+}
+
+# Créer le container avec volume pour le live editing
 resource "docker_container" "web" {
   name  = "demo-devops-container"
   image = docker_image.web.image_id
 
   ports {
     internal = 80
-    external = 8081
+    external = 8081  # port libre
   }
 
+  # Volume pour synchroniser les fichiers locaux avec le container
   volumes {
-    host_path      = "${path.module}/html"  # dossier local
-    container_path = "/usr/share/nginx/html"  # dossier Nginx dans le container
+    host_path      = "${path.module}/html"           # dossier local
+    container_path = "/usr/share/nginx/html"        # dossier servi par Nginx
   }
 }
